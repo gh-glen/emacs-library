fe1d7386f2207bd7d31db1e3fd41971fae4a69a9
Glen Pritchett <glen-github@hungry.com>
2010-09-13 19:07:43 -0700

    Removed some older obsolete things, and cleaned up organization a bit.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,24 +11,8 @@
 	      (list "~/.emacs.d/library/elisp")))        ;; general elisp
 
 
-;;----------------------------
-;; server mode
-;(server-start)
-
-
-;;------------------------------
-;; Additional Packages
-; org-mode
-(setq org-startup-folded "showall")
-(setq org-todo-keywords 
-      (quote ((sequence "TODO(t!)" "WAITING(w@)" "DONE(d!)"))))
-
-
 ;;------------------------------
 ;; Set up some nice "default" behavior
-(autoload 'add-autohook "autohook" "Add load hook to a package.")
-(autoload 'autoloads-for-package "autohook" "Find all entry points of a package.")
-;(autoload 'gnus "gnus" "The gnus NNTP news reading service" t)
 (setq inhibit-startup-message t)
 (setq line-move-visual nil) ; please don't foul up my macros, emacs 23!
 (display-time)
@@ -40,16 +24,7 @@
 (global-font-lock-mode t)
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 (add-hook 'TeX-mode-hook 'turn-on-auto-fill)
-
-
-;;----------------------------
-;; use diary and appt 
-(setq diary-file "~/.emacs.d/diary")
-; note that appt requires (display-time) to work
-(add-hook 'diary-hook 'appt-make-list)
-(setq appt-audible nil)
-(setq appt-msg-window nil)
-(diary 7)
+;;(server-start)
 
 
 ;;----------------------------
@@ -57,7 +32,25 @@
 (global-set-key "\b" 'delete-backward-char)
 (global-set-key "\M-t" 'goto-line)
 (global-set-key "\M-c" 'compile)
-;(global-set-key "\C-x\C-n" 'gnus) ;;; Load GNUS and bind C-X C-N to read news
+
+
+;;------------------------------
+;; Additional Packages
+;; (always)
+;;org-mode
+(setq org-startup-folded "showall")
+(setq org-todo-keywords 
+      (quote ((sequence "TODO(t!)" "WAITING(w@)" "DONE(d!)"))))
+;;diary and appt 
+(setq diary-file "~/.emacs.d/diary")
+;; note that appt requires (display-time) to work
+(add-hook 'diary-hook 'appt-make-list)
+(setq appt-audible nil)
+(setq appt-msg-window nil)
+(diary 7)
+;; (sometimes)
+;;php-mode
+(autoload 'php-mode "php-mode" "Major mode for editing PHP." t)
 
 
 ;;----------------------------
@@ -83,5 +76,5 @@
       (append '(("\\.php$"   . php-mode))
               auto-mode-alist))
 
-;; the end.
 
+;; the end.
